fb3c222a37004d845049c0ee027c93606ceba676
lnngle <lnngle@163.com>
2020-11-22 23:27:54 +0800

    dev

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -3,14 +3,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { join } from 'path';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
 
-console.debug("APP_ENV:" + process.env.APP_ENV);
+console.log("APP_ENV:" + process.env.APP_ENV);
 
-const envFilePath = process.env.APP_ENV === 'dev' ?
-join(process.cwd(), 'environment', 'dev.env') :
-join(process.cwd(), 'environment', 'prod.env');
-console.debug("envFilePath:" + envFilePath);
+const MY_ENV_FILENAME = process.env.APP_ENV === 'prod' ? 'prod.env' : 'dev.env';
+console.log("MY_ENV_FILENAME:" + MY_ENV_FILENAME);
 
 @Module({
   imports: [
@@ -18,10 +17,22 @@ console.debug("envFilePath:" + envFilePath);
       encoding: 'utf-8',
       isGlobal: true,
       ignoreEnvFile: false,
-      envFilePath: ['.env', './apps/api/environment/dev.env'],
-    })
+      envFilePath: ['.env', './apps/api/environment/' + MY_ENV_FILENAME],
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      entities: [],
+      synchronize: true,
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private readonly configService: ConfigService) {}
+}
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -7,15 +7,9 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
-import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  const configService = app.get(ConfigService);
-  console.log(configService);
-  console.log(configService.get<string>('APP_ENV'));
-  console.log(configService.get<string>('aa'));
 
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
